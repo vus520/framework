a87b291437c5b9493e2a66bc1f6d96bbd9258b3a
尘缘 <admin@4wei.cn>
2016-05-11 12:27:18 +0800

    优化单元测试

--- tests/thinkphp/library/think/cache/driver/redisdTest.php
+++ tests/thinkphp/library/think/cache/driver/redisdTest.php
@@ -15,7 +15,7 @@ namespace tests\thinkphp\library\think\cache\driver;
  * Redisd缓存驱动测试
  * @author 尘缘 <130775@qq.com>
  */
-class redisdTest extends cacheTestCase
+class redisdTest extends redisTest
 {
     private $_cacheInstance = null;
 
@@ -24,7 +24,6 @@ class redisdTest extends cacheTestCase
         if (!extension_loaded("redis")) {
             $this->markTestSkipped("Redis没有安装，已跳过测试！");
         }
-        \think\Cache::connect(array('type' => 'redis', 'expire' => 2));
     }
 
     protected function getCacheInstance()
@@ -35,38 +34,14 @@ class redisdTest extends cacheTestCase
         return $this->_cacheInstance;
     }
 
-    public function testGet()
-    {
-        $cache = $this->prepare();
-        $this->assertEquals('string_test', $cache->get('string_test'));
-        $this->assertEquals(11, $cache->get('number_test'));
-        $result =  $cache->get('array_test');
-        $this->assertEquals('array_test', $result['array_test']);
-    }
-
     public function testStoreSpecialValues()
     {
+        parent::testStoreSpecialValues();
+        
         $redis = $this->getCacheInstance();
-        $redis->master(true);
-
-        $redis->handler()->setnx('key', 'value');
-        $value = $redis->handler()->get('key');
-        $this->assertEquals('value', $value);
         
         $redis->master(true)->set('key', 'val');
         $value = $redis->master(false)->get('key');
         $this->assertEquals('val', $value);
-        
-        $redis->handler(true)->hset('hash', 'key', 'value');
-        $value = $redis->handler(false)->hget('hash', 'key');
-        $this->assertEquals('value', $value);
-    }
-
-    public function testExpire()
-    {
-    }
-    
-    public function testQueue()
-    {
     }
 }
